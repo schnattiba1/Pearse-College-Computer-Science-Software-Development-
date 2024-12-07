609c01ab608b751ce96083c12fc2e077171c3109
Chanel <81082592+schnattiba1@users.noreply.github.com>
2024-12-07 18:44:58 +0000

    Update Main.java

--- fundamentals of oop/assignment2/Main.java
+++ fundamentals of oop/assignment2/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Invoking the methods
+        // Calling the method
         programIntroduction();
         studentScore();
     }
@@ -19,11 +19,14 @@ public class Main {
 
         // Asking the student's full name
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter your full name: ");
-        String fullName = input.next();
+        System.out.print("Please enter your first name: ");
+        String firstName = input.next();
+
+        System.out.print("Please enter your second name: ");
+        String secondName = input.next();
 
         // Welcoming the student and introduction
-        System.out.println(System.lineSeparator() + "Hello, " + fullName + ".");
+        System.out.println(System.lineSeparator() + "Hello, " + firstName + " " + secondName +  ".");
         System.out.println(System.lineSeparator() + "Welcome to the calculation score program.");
         System.out.println(System.lineSeparator() + "In this calculation score program, we calculate the score of each distinction, merit or poor exam scores all together.");
     }
@@ -39,46 +42,62 @@ public class Main {
             int score1 = input.nextInt();
 
             // Condition statement -> checking if the number is a range or not
-            if (score1 <= 0 || score1 >= 100) {
-                System.out.println(System.lineSeparator() + "Please enter score for Maths for IT");
-                int retypeScore = input.nextInt();
+            if (!(score1 >= 0 && score1 <= 100)) {
+                System.out.println(System.lineSeparator() + "You entered a inappropriate range!");
+                closeProgram();
+                break;
             }
 
             System.out.println(System.lineSeparator() + "Please enter score for Database Methods");
             int score2 = input.nextInt();
 
-            if (score2 <= 0 || score2 >= 100) {
-                System.out.println(System.lineSeparator() + "Please enter score for Database Methods");
-                int retypeScore = input.nextInt();
+            if (!(score2 >= 0 && score2 <= 100)) {
+                System.out.println(System.lineSeparator() + "You entered a inappropriate range!");
+                closeProgram();
+                break;
             }
 
             System.out.println(System.lineSeparator() + "Please enter score for Programming Design Principles");
             int score3 = input.nextInt();
 
-            if (score3 <= 0 || score3 >= 100) {
-                System.out.println(System.lineSeparator() + "Please enter score for Programming Design Principles");
-                int retypeScore = input.nextInt();
+            if (!(score3 >= 0 && score3 <= 100)) {
+                System.out.println(System.lineSeparator() + "You entered a inappropriate range!");
+                closeProgram();
+                break;
             }
 
             System.out.println(System.lineSeparator() + "Please enter score for Fundamentals of OOP");
             int score4 = input.nextInt();
 
-            if (score4 <= 0 || score4 >= 100) {
-                System.out.println(System.lineSeparator() + "Please enter score for Fundamentals of OOP");
-                int retypeScore = input.nextInt();
+            if (!(score4 >= 0 && score4 <= 100)) {
+                System.out.println(System.lineSeparator() + "You entered a inappropriate range!");
+                closeProgram();
+                break;
             }
 
             System.out.println(System.lineSeparator() + "Please enter score for Software Architecture");
             int score5 = input.nextInt();
 
-            if (score5 <= 0 || score5 >= 100) {
-                System.out.println(System.lineSeparator() + "Please enter score for Software Architecture");
-                int retypeScore = input.nextInt();
+            if (!(score5 >= 0 && score5 <= 100)) {
+                System.out.println(System.lineSeparator() + "You entered a inappropriate range!");
+                closeProgram();
+                break;
             }
+            
             int result = ((score1 + score2 + score3 + score4 + score5)) / 5;
             System.out.println(System.lineSeparator() + "Your average score: " + result);
-            closeProgram();
-            break;
+
+            System.out.println(System.lineSeparator() + "Would you like to enter your scores again? Yes(Y)/No(N) ");
+            String enterScoresAgain = input.next();
+            
+            // Checks if the student entered a 'Yes' or a 'No'
+            if (enterScoresAgain.matches("Yes") || enterScoresAgain.matches("Y")) {
+                continue;
+            }
+            if (enterScoresAgain.matches("No") || enterScoresAgain.matches("N")) {
+                closeProgram();
+                break;
+            }
         }
     }
 }
